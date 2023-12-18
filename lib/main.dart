b713f49db0497812d399b49840d97f7560684167
unknown <meroo1995saggaf@outlook.com>
2023-12-18 13:37:30 +0300

    thread clone project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:thread_clone/firebase_options.dart';
+import 'package:thread_clone/screens/home.dart';
+
+import 'screens/login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return const Home();
+            } else {
+              return const LoginScreen();
+            }
+          }),
+    );
+  }
+}
